3aa3d4abfd1ecebc77d14acca7fb408315a8e012
Nicki Skafte <skaftenicki@gmail.com>
2025-04-30 11:04:30 +0200

    add project plan

---
+++ tasks.py
@@ -0,0 +1,15 @@
+from invoke import task, Context
+
+
+@task
+def clean(ctx: Context):
+    """Clean the project by removing build artifacts."""
+    ctx.run("rm -rf build dist *.egg-info")
+    ctx.run("rm -rf project_name")
+    ctx.run("rm -rf .ruff_cache")
+
+
+@task
+def template(ctx: Context):
+    """Generate a new project from template."""
+    ctx.run("uv run cookiecutter . -f --no-input")
